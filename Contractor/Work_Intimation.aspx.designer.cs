f84b7d4671f4ac52b06681db2628bf066ef179d1
Navneet1028 <139944384+Navneet1028@users.noreply.github.com>
2023-09-15 16:53:00 +0530

    Changes 16:53

--- Contractor/Work_Intimation.aspx.designer.cs
+++ Contractor/Work_Intimation.aspx.designer.cs
@@ -24,15 +24,6 @@ namespace CEIHaryana.Contractor
         protected global::System.Web.UI.ScriptManager ScriptManager1;
 
         /// <summary>
-        /// UpdatePanel1 control.
-        /// </summary>
-        /// <remarks>
-        /// Auto-generated field.
-        /// To modify move field declaration from designer file to code-behind file.
-        /// </remarks>
-        protected global::System.Web.UI.UpdatePanel UpdatePanel1;
-
-        /// <summary>
         /// DataUpdated control.
         /// </summary>
         /// <remarks>
@@ -49,6 +40,15 @@ namespace CEIHaryana.Contractor
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
         protected global::System.Web.UI.HtmlControls.HtmlGenericControl DataSaved;
+
+        /// <summary>
+        /// UpdatePanel1 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.UpdatePanel UpdatePanel1;
 
         /// <summary>
         /// ddlworktype control.
