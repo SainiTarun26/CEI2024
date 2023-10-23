091c878710c6ffc34417c7d33c6d7aeb682f9961
Navneet1028 <139944384+Navneet1028@users.noreply.github.com>
2023-10-23 12:22:31 +0530

    Changes

--- Supervisor/SupervisorGeneraterSetTestReport.aspx.cs
+++ Supervisor/SupervisorGeneraterSetTestReport.aspx.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using CEI_PRoject;
+using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Web;
 using System.Web.UI;
@@ -9,9 +11,71 @@ namespace CEIHaryana.Supervisor
 {
 	public partial class SupervisorGeneraterSetTestReport : System.Web.UI.Page
 	{
-		protected void Page_Load(object sender, EventArgs e)
-		{
+        CEI cei = new CEI();
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack)
+            {
+                GridviewBind();
+            }
+        }
 
-		}
-	}
+        protected void GridviewBind()
+        {
+            string LoginId = string.Empty;
+            LoginId = Session["AdminID"].ToString();
+            DataSet ds = new DataSet();
+            ds = cei.GetGeneraterSetData(LoginId);
+            if (ds.Tables.Count > 0)
+            {
+                GridView1.DataSource = ds;
+                GridView1.DataBind();
+            }
+            else
+            {
+                GridView1.DataSource = null;
+                string script = "alert(\"No Record Found\");";
+                ScriptManager.RegisterStartupScript(this, GetType(), "serverScript", script, true);
+            }
+            ds.Dispose();
+        }
+
+        protected void GridView1_RowCommand(object sender, GridViewCommandEventArgs e)
+        {
+            if (e.CommandName == "Select")
+            {
+                Control ctrl = e.CommandSource as Control;
+                GridViewRow row = ctrl.Parent.NamingContainer as GridViewRow;
+                Label lblID = (Label)row.FindControl("lblID");
+                string id = lblID.Text;
+                Session["LineID"] = id;
+                Label lblApproval = (Label)row.FindControl("lblApproval");
+                Session["Approval"] = lblApproval.Text;
+                if (e.CommandName == "Select")
+                {
+                    Response.Redirect("/TestReportModal/LineTestReportModal.aspx");
+                }
+            }
+        }
+
+        protected void txtSearch_TextChanged(object sender, EventArgs e)
+        {
+            string searhterm = txtSearch.Text.Trim();
+            string LoginId = string.Empty;
+            LoginId = Session["AdminID"].ToString();
+
+            DataSet ds = new DataSet();
+            ds = cei.SearchingOnGeneraterSet(searhterm, LoginId);
+            if (ds.Tables.Count > 0)
+            {
+                GridView1.DataSource = ds;
+                GridView1.DataBind();
+            }
+            else
+            {
+                string script = "alert(\"No Record Match\");";
+                ScriptManager.RegisterStartupScript(this, GetType(), "Server Script", script, true);
+            }
+        }
+    }
 }
