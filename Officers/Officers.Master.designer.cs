48fe14d2a3f1be0d19b16a5df6185c8ff2469b35
hp <hp@DESKTOP-KE574VS>
2023-12-12 17:41:03 +0530

    5:40

--- Officers/Officers.Master.designer.cs
+++ Officers/Officers.Master.designer.cs
@@ -42,15 +42,6 @@ namespace CEIHaryana.Officers
         protected global::System.Web.UI.WebControls.Label lblName;
 
         /// <summary>
-        /// Label1 control.
-        /// </summary>
-        /// <remarks>
-        /// Auto-generated field.
-        /// To modify move field declaration from designer file to code-behind file.
-        /// </remarks>
-        protected global::System.Web.UI.WebControls.Label Label1;
-
-        /// <summary>
         /// btnLogout control.
         /// </summary>
         /// <remarks>
@@ -58,15 +49,6 @@ namespace CEIHaryana.Officers
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
         protected global::System.Web.UI.WebControls.Button btnLogout;
-
-        /// <summary>
-        /// Button1 control.
-        /// </summary>
-        /// <remarks>
-        /// Auto-generated field.
-        /// To modify move field declaration from designer file to code-behind file.
-        /// </remarks>
-        protected global::System.Web.UI.WebControls.Button Button1;
 
         /// <summary>
         /// ContentPlaceHolder1 control.
