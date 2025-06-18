f86a3e534d0ec8a4a757d20fcde4cf1bc4bb5492
Navneet1028 <navneet10a28@gmail.com>
2025-06-18 17:46:49 +0530

    Neha Changes 2 New Regestration 18-June-2025

---
+++ ToVerifyRegistration.aspx
@@ -0,0 +1,19 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="ToVerifyRegistration.aspx.cs" Inherits="CEIHaryana.ToVerifyRegistration" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+           <p class="style1">
+    We are pleased to inform you that your account has been successfully created. Please check your email for your ID and password.<br /><br />
+    <a href="https://ceiharyana.com" target="_blank">Click here to proceed with the further process</a>.
+</p>
+        </div>
+    </form>
+</body>
+</html>
---
+++ ToVerifyRegistration.aspx.cs
@@ -0,0 +1,66 @@
+﻿using CEI_PRoject;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace CEIHaryana
+{
+    public partial class ToVerifyRegistration : System.Web.UI.Page
+    {
+        //Page Created by neha 18-June-2025
+        CEI CEI = new CEI();
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            string encryptedId = Request.QueryString["id"];
+            if (!string.IsNullOrEmpty(encryptedId))
+            {
+                try
+                {
+                    string email = Request.QueryString["email"]; 
+                    if (!string.IsNullOrEmpty(email))
+                    {
+                        string decryptedRandomNumber = DecryptRandomNumber(encryptedId);
+
+                        DataTable dt = CEI.GetNewAccountCredentials(decryptedRandomNumber);
+
+                        if (dt != null && dt.Rows.Count > 0)
+                        {
+                            string UserId = dt.Rows[0]["UserId"].ToString();
+                            string Password = dt.Rows[0]["Password"].ToString();
+
+                            CEI.NewAccountCredentialsthroughEmail(email, UserId, Password);                           
+                        }
+                        else
+                        {
+                        }
+                        
+
+                        //CEI.NewAccountCredentialsthroughEmail(email);
+                    }
+                }
+                catch (FormatException ex)
+                {
+                    Response.Write("Invalid ID format.");
+                }
+            }
+        }
+
+        private string DecryptRandomNumber(string encryptedId)
+        {
+            int padding = 4 - (encryptedId.Length % 4);
+            if (padding != 4)
+            {
+                encryptedId = encryptedId.PadRight(encryptedId.Length + padding, '=');
+            }
+
+            byte[] bytes = Convert.FromBase64String(encryptedId);
+            return Encoding.UTF8.GetString(bytes);
+        }
+    }
+}
