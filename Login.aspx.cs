36d6244e3d2338b279682f9d1bd3efc4b8f52b00
Navneet1028 <navneet10a28@gmail.com>
2025-10-01 17:59:54 +0530

    Neha code

--- Login.aspx.cs
+++ Login.aspx.cs
@@ -368,13 +368,21 @@ namespace CEIHaryana
                                     Session["ContractorID"] = txtUserID.Text;
                                     if (ApplicationStatus.Trim() == "New")
                                     {
-                                        Response.Redirect("/UserPages/ContractorApplicationForm.aspx", false);
+                                        int result = Convert.ToInt32(cei.DetailOfContractorExist(txtUserID.Text));
+                                        if (result == 1)
+                                        {
+                                            Response.Redirect("/UserPages/Update_Contractor_Application_Form.aspx", false);
+                                        }
+                                        else if (result == 0)
+                                        {
+                                            Response.Redirect("/UserPages/ContractorApplicationForm.aspx", false);
+                                        }
                                     }
                                     else if (ApplicationStatus.Trim() == "Mid")
                                     {
                                         Response.Redirect("/UserPages/DocumentsForContractor.aspx", false);
                                     }
-                                    else
+                                    else if (ApplicationStatus.Trim() == "Old")
                                     {
                                         Response.Redirect("/UserPages/New_Application_Status.aspx", false);
                                     }
